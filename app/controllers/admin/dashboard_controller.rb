e9edd5e4a30c131da1d031d5ee8fffe306830a8c
ArturT <mr.arturio@gmail.com>
2023-07-06 21:21:30 -0400

    Admin Dashboard Counts using fetch

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,21 @@
 class Admin::DashboardController < ApplicationController
   def show
+    # Using AR models in the ERB view 
+    # @product_count =  Product.count
+    # @category_count = Category.count
+
+      # Fetch the data in the controller and assign it to instance variables
+      @product_count = fetch_product_count
+      @category_count = fetch_category_count
+  end
+  
+  private
+
+  def fetch_product_count
+    Product.count
+  end
+
+  def fetch_category_count
+    Category.count
   end
 end
